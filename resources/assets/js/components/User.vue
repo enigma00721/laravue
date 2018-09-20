64716cb0329127f37e34520bf1e772d6c0614611
I Sewa <web@isewa.com.np>
2018-09-20 09:17:08 +0545

    secon commit with visual studio

---
+++ resources/assets/js/components/User.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="container mt-5" >
+        <div class="row" >
+          <div class="col-md-12" >
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">User Table</h3>
+
+                <div class="card-tools">
+                    <button class="btn btn-success" data-toggle="modal"
+                             data-target="#addNewUser" type="button">
+                        Add New
+                         <i class="fa fa-user-plus text-white"></i>
+                    </button>
+                </div>
+
+                
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody><tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Modify</th>
+                  </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>
+                        <a href="#">
+                            <i class="fa fa-pencil text-indigo" aria-hidden="true"></i>
+                        </a>
+                        /
+                        <a href="#">
+                            <i class="fa fa-trash text-red" aria-hidden="true"></i>
+                        </a>
+                    </td>
+                  </tr>
+                 
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+
+
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="addNewUser" tabindex="-1" role="dialog" aria-labelledby="addNewUserLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="addNewUserLabel">Add New</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                
+                <div class="form-group">
+                     <input v-model="form.name" type="text" name="name" placeholder="Enter Name" 
+                       class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                     <has-error :form="form" field="name"></has-error>
+                </div>
+                <div class="form-group">
+                     <input v-model="form.email" type="text" name="email" placeholder="Email Address" 
+                       class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                     <has-error :form="form" field="email"></has-error>
+                </div>
+                 <div class="form-group">
+                     <textarea v-model="form.bio" name="bio" placeholder="Bio" 
+                       class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }">
+                   </textarea>
+                     <has-error :form="form" field="bio"></has-error>
+                </div>
+
+                <div class="form-group">
+                    <select name="type" id="type" v-model="form.type"  class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                        <option value="">Select User Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                        <option value="author">Author</option>
+                    </select>
+                     <has-error :form="form" field="email"></has-error>
+                </div>
+
+                 <div class="form-group">
+                     <input v-model="form.password" type="password" name="password" placeholder="Password" 
+                       class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                     <has-error :form="form" field="password"></has-error>
+                </div>
+               
+
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Create</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- modal end -->
+
+    </div>
+    <!-- container end -->
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                form : new Form({
+                    name : '' ,
+                    email : '' ,
+                    password : '' ,
+                    type : '' ,
+                    bio : '' ,
+                    photo : '' ,
+                    name : '' 
+                })
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,166 @@
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+  <title>LaraVue</title>
+
+
+  <link rel="stylesheet" href="css/app.css">
+</head>
+<body class="hold-transition sidebar-mini" >
+<div class="wrapper" id="app">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+
+  <ul class="navbar-nav">
+    <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+          </li>
+  </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+  
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="{{asset('image/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">LaraVue</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{asset('image/man.png')}}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <router-link to="/dashboard" class="nav-link">
+              <i class="nav-icon fa fa-dashboard text-purple"></i>
+              <p>
+                Dashboard
+               
+              </p>
+            </router-link>
+          </li>
+          <li class="nav-item has-treeview ">
+            <a href="#" class="nav-link ">
+              <i class="nav-icon fa fa-cog text-cyan"></i>
+              <p>
+                Management
+                <i class="right fa fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <router-link to="/user" class="nav-link">
+                  <i class="fa fa-user-o nav-icon"></i>
+                  <p>User</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="fa fa-circle-o nav-icon"></i>
+                  <p>Inactive Page</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link to="/profile"
+               class="nav-link {{ Request::is('/home') ? 'active' : '' }}" >
+              <i class="nav-icon fa fa-user text-green"></i>
+              <p >
+                Profile
+               
+              </p>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit(); ">
+              <i class="nav-icon fa fa-power-off text-orange"></i>
+              <p>
+                Logout
+               
+              </p>
+            </a>
+            <form action="{{ route('logout') }}" id="logout-form" method="post" style="display:none;">
+              @csrf
+            </form>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+   <div class="content">
+     <div class="container-fluid">
+       <router-view></router-view>  
+     </div>
+   </div>
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">LaraVue</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- AdminLTE App -->
+<script src="js/app.js"></script>
+</body>
+</html>
